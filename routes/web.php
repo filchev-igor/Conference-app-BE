f1f79f0b15b83b23fbc740aad938eaf349061485
Igor Filchev <filchev.igor@gmail.com>
2024-11-03 22:14:52 +0200

    REST API

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('api')->group(function () {
-    Route::get('/conferences', [ConferenceController::class, 'index']);
-    Route::post('/conferences', [ConferenceController::class, 'store']);
-    Route::get('/conferences/{id}', [ConferenceController::class, 'show']);
-    Route::put('/conferences/{id}', [ConferenceController::class, 'update']);  // Use PUT instead of PATCH
-    Route::delete('/conferences/{id}', [ConferenceController::class, 'destroy']);
+Route::get('/conferences', [ConferenceController::class, 'index']);
+Route::post('/conferences', [ConferenceController::class, 'store']);
+Route::get('/conferences/{id}', [ConferenceController::class, 'show']);
+Route::put('/conferences/{id}', [ConferenceController::class, 'update']);  // Use PUT instead of PATCH
+Route::delete('/conferences/{id}', [ConferenceController::class, 'destroy']);
 
-    Route::get('/users', [UserController::class, 'index']);
-    Route::get('/users/{id}', [UserController::class, 'show']);  // Add this route for user details
-    Route::put('/users/{id}', [UserController::class, 'update']);  // Use PUT instead of PATCH
-});
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);  // Add this route for user details
+Route::put('/users/{id}', [UserController::class, 'update']);  // Use PUT instead of PATCH
 
