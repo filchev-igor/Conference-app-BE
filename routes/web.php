99162fcc68f0a06bfabf78a94048ac6ce7e7dd37
Igor Filchev <filchev.igor@gmail.com>
2024-11-03 21:59:05 +0200

    REST API

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,22 @@
 <?php
 
+use App\Http\Controllers\ConferenceController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users/${id}', function () {
-    //role
-    //theme
-    //conferences
+Route::middleware('api')->group(function () {
+    Route::get('/conferences', [ConferenceController::class, 'index']);
+    Route::post('/conferences', [ConferenceController::class, 'store']);
+    Route::get('/conferences/{id}', [ConferenceController::class, 'show']);
+    Route::put('/conferences/{id}', [ConferenceController::class, 'update']);  // Use PUT instead of PATCH
+    Route::delete('/conferences/{id}', [ConferenceController::class, 'destroy']);
+
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'show']);  // Add this route for user details
+    Route::put('/users/{id}', [UserController::class, 'update']);  // Use PUT instead of PATCH
 });
 
-Route::patch('/users/${id}', function () {
-    //role
-    //theme
-});
-
-Route::get('/users', function () {
-    //data for the list of users
-});
-
-
-Route::get('/conferences', function () {
-    //data for the list of conferences
-});
-
-Route::post('/conferences', function () {
-    //add new conference
-});
-
-Route::patch('/conferences/${id}', function () {
-    //register user for the conference
-    //add conference to the user list
-});
