22530c1314ea661402127d4a1cfd9e2b705247ae
Igor Filchev <filchev.igor@gmail.com>
2024-11-04 18:19:41 +0200

    api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,11 +7,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        /*
+        $middleware->validateCsrfTokens(except: [
+            'https://localhost:5173/Conference-app-FE/',
+        ]);
+        */
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
